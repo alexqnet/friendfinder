f5fc4842f50d39e30dc6323efd72eff28f5a2f2d
Alexander Quiñones <alex@Alex.local>
2017-08-31 22:47:27 -0400

    upload

---
+++ api/data/friends.js
@@ -0,0 +1,176 @@
+var friendsArray = [
+
+{
+"name": "Adam Scott",
+"photo": "../assets/adam-scott.jpg",
+"scores": [
+1,
+"2",
+"3",
+"4",
+"5",
+"1",
+"2",
+"3",
+"4",
+"5"
+]
+},
+
+{
+"name": "Chris Pine",
+"photo": "../assets/chris-pine.jpg",
+"scores": [
+"5",
+"4",
+"3",
+"2",
+"1",
+"1",
+"2",
+"3",
+"4",
+"5"
+]
+},
+
+{
+"name": "Danny Trejo",
+"photo": "../assets/danny-trejo.jpg",
+"scores": [
+"2",
+"3",
+"4",
+"5",
+"1",
+"2",
+"3",
+"4",
+"5",
+"1"
+]
+},
+
+{
+"name": "Hugh Grant",
+"photo": "../assets/hugh-grant.jpg",
+"scores": [
+"3",
+"4",
+"5",
+"1",
+"2",
+"3",
+"4",
+"5",
+"1",
+"2"
+]
+},
+
+{
+"name": "Ian McKellen",
+"photo": "../assets/ian-mckellen.jpg",
+"scores": [
+"1",
+"2",
+"5",
+"5",
+"3",
+"2",
+"3",
+"3",
+"4",
+"4"
+]
+}, 
+
+{
+"name": "John Goodman",
+"photo": "../assets/John Goodman.jpg",
+"scores": [
+"4",
+"5",
+"1",
+"2",
+"3",
+"4",
+"5",
+"1",
+"2",
+"3"
+]
+}, 
+
+{
+"name": "Leo DiCaprio",
+"photo": "../assets/leo-dicaprio.jpg",
+"scores": [
+"3",
+"1",
+"1",
+"4",
+"5",
+"3",
+"3",
+"5",
+"5",
+"1"
+]
+}, 
+
+{
+"name": "Matthew McConaughey",
+"photo": "../assets/matthew-mcconaughey.jpg",
+"scores": [
+"1",
+"3",
+"5",
+"2",
+"4",
+"4",
+"2",
+"5",
+"3",
+"1"
+]
+}, 
+
+{
+"name": "Ryan Gosling",
+"photo": "../assets/ryan-gossling.jpg",
+"scores": [
+"4",
+"5",
+"2",
+"1",
+"4",
+"5",
+"4",
+"2",
+"3",
+"2"
+]
+}, 
+
+{
+"name": "Simon Pegg",
+"photo": "../assets/simon-pegg.jpg",
+"scores": [
+"4",
+"5",
+"2",
+"3",
+"5",
+"2",
+"3",
+"1",
+"1",
+"5"
+]
+}
+
+
+]
+
+module.exports = friendsArray;
